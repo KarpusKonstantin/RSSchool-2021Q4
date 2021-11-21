9b372b6f08fb0308d058172492beac03cd196b0b
Kostik <ks@wintecs.by>
2021-11-21 21:14:22 +0300

    feat: add test-write.txt file form read/write tests

--- tests/myStream/myWriteStream.test.js
+++ tests/myStream/myWriteStream.test.js
@@ -1,13 +1,17 @@
 const { Writable } = require('stream');
 
+const path = require('path');
+const fs = require('fs');
+
 const MyWriteStream = require("../../src/myStream/myWriteStream");
 
 describe('Write stream test', () => {
-  const OUTPUT_FILE_PATH = './output.txt';
+  const TEST_WRITE_FILE = path.resolve(__dirname,  '..', '..',  'tests/myStream/test-write.txt');
+  const writeObject = new MyWriteStream(TEST_WRITE_FILE);
 
   test('should be instance of Writable stream', ()=>{
-    const writeObject = new MyWriteStream(OUTPUT_FILE_PATH);
 
     expect( writeObject instanceof Writable).toEqual(true);
   });
+
 });
